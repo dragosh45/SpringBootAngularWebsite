06362363f9fa3c522607010945961881cfa5b88f
dragosh45 <dragos.preda20@yahoo.com>
2023-03-09 15:26:32 +0200

    Added Checkout Form dropp down lists.

---
+++ src/main/java/com/myapps/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,4 @@
+package com.myapps.ecommerce.dao;
+
+public interface CountryRepository extends org.springframework.data.jpa.repository.JpaRepository<com.myapps.ecommerce.entity.Country, int> {
+}
---
+++ src/main/java/com/myapps/ecommerce/dao/StateRepository.java
@@ -0,0 +1,4 @@
+package com.myapps.ecommerce.dao;
+
+public interface StateRepository extends org.springframework.data.jpa.repository.JpaRepository<com.myapps.ecommerce.entity.State, int> {
+}
